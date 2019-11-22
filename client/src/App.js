7f3aee7f8dd12ff716d89be602f721ed75a7d1b0
dbell <greenadvisor@gmail.com>
2019-11-22 18:06:45 -0500

    schema updated, nav and jumbotron displaying, api routes modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
+import SearchBooks from "./pages/SearchBooks";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
 
 function App() {
   return (
     <Router>
       <div>
         <Nav />
+        <Jumbotron />
         <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/books" component={SearchBooks} />
           <Route exact path="/books/:id" component={Detail} />
           <Route component={NoMatch} />
         </Switch>
---
+++ client/src/pages/SearchBooks.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+
+import API from "../utils/API";
+import Navbar from "../components/Nav";
+
+class SearchBooks extends Component {
+  state = {
+    books: [],
+  };
+
+  // componentDidMount() {
+  //   this.loadBooks();
+  // }
+
+  // loadBooks = () => {
+  //   API.getBooks()
+  //     .then(res =>
+  //       this.setState({ books: res.data })
+  //     )
+  //     .catch(err => console.log(err));
+  // };
+
+
+  // todo: move to saved page when ready
+  // deleteBook = id => {
+  //   API.deleteBook(id)
+  //     .then(res => this.loadBooks())
+  //     .catch(err => console.log(err));
+  // };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  //todo: booksearch function - figure out url from api, write function
+
+    //todo: (savebook function) modify to take event targets and add elements from google books api?
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   if (this.state.title && this.state.author) {
+  //     API.saveBook({
+  //       title: this.state.title,
+  //       author: this.state.author,
+  //       synopsis: this.state.synopsis
+  //     })
+  //       .then(res => this.loadBooks())
+  //       .catch(err => console.log(err));
+  //   }
+  // };
+
+  render() {
+    return (
+      <div>
+
+        </div>
+    );
+  }
+}
+
+export default SearchBooks;
